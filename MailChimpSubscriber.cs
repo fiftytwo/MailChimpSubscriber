8a5e048b5f55486a33400eb5a753b581890751c0
Dmitry Victorov <dmitry.viktorov@gmail.com>
2020-03-20 20:10:55 +0300

    - Use UnityWebRequest instead of WWW - Exclude logging from release builds

--- MailChimpSubscriber.cs
+++ MailChimpSubscriber.cs
@@ -1,29 +1,32 @@
+﻿#if UNITY_EDITOR || DEVELOPMENT_BUILD
+#define ENABLE_MAILCHIMP_ERROR_LOG
+#define ENABLE_MAILCHIMP_SUCCESS_LOG
+#endif
+
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 using System;
-using System.Collections.Generic;
 using System.Collections;
 using System.Text;
 using System.Net.Mail;
+using JetBrains.Annotations;
+using UnityEngine.Networking;
 
 
 namespace Fiftytwo
 {
+    [PublicAPI]
     public class MailChimpSubscriber : MonoBehaviour
     {
         private const string UrlFormat = "https://{0}.api.mailchimp.com/3.0/lists/{1}/members";
-        private const string DataFormat = "{{\"email_address\":\"{0}\", \"status\":\"subscribed\"}}";
+        private const string JsonFormat = "{{\"email_address\":\"{0}\", \"status\":\"subscribed\"}}";
 
-        [SerializeField]
-        public MailChimpEvent SubscribeSuccess;
-        [SerializeField]
-        public MailChimpEvent SubscribeError;
+        public MailChimpEvent SubscribeSuccess = new MailChimpEvent();
+        public MailChimpEvent SubscribeError = new MailChimpEvent();
 
-        [SerializeField]
-        private string _apiKey;
-        [SerializeField]
-        private string _listId;
+        [SerializeField] private string _apiKey = string.Empty;
+        [SerializeField] private string _listId = string.Empty;
 
 
         public void Subscribe ()
@@ -32,93 +35,94 @@ namespace Fiftytwo
 
             if( text == null )
             {
+#if ENABLE_MAILCHIMP_ERROR_LOG
                 Debug.LogError( "MailChimp — No UI Text found at this GameObject" );
+#endif
+                SubscribeError.Invoke( null );
                 return;
             }
-            else
-            {
-                Subscribe( text.text );
-            }
+
+            Subscribe( text.text );
         }
 
         public void Subscribe ( string email )
         {
-            if( IsValidEmail( email ) )
-            {
-                StartCoroutine( SendToMailChimp( email ));
-            }
-            else
-            {
-                Debug.Log( "MailChimp — Invalid email" );
-                SubscribeError.Invoke( email );
-            }
-        }
-
-        private bool IsValidEmail ( string email )
-        {
-            if( string.IsNullOrEmpty( email ) )
-                return false;
-
             try
             {
-                new MailAddress( email );
+                var url = BuildUrl();
+                if( string.IsNullOrEmpty( url ) )
+                {
+#if ENABLE_MAILCHIMP_ERROR_LOG
+                    Debug.LogError( "MailChimp — Invalid API Key format" );
+#endif
+                    SubscribeError.Invoke( email );
+                    return;
+                }
 
-                return true;
+                var mailAddress = new MailAddress( email );
+
+                StartCoroutine( SendToMailChimp( url, mailAddress.Address ) );
             }
-            catch( FormatException )
+            catch( Exception ex )
             {
-                return false;
-            }
-        }
-
-        private IEnumerator SendToMailChimp ( string email )
-        {
-            var www = BuildWWW( email );
-
-            if( www == null )
-            {
-                Debug.Log( "MailChimp — Subscribe error: can't build request" );
+#if ENABLE_MAILCHIMP_ERROR_LOG
+                Debug.LogError( "MailChimp — Invalid email: " + ex.Message );
+#endif
                 SubscribeError.Invoke( email );
             }
-            else
-            {
-                yield return www;
-
-                if( string.IsNullOrEmpty( www.error ) )
-                {
-                    Debug.Log( "MailChimp — Subscribe success" );
-                    SubscribeSuccess.Invoke( email );
-                }
-                else
-                {
-                    Debug.Log( "MailChimp — Subscribe error: " + www.error );
-                    SubscribeError.Invoke( email );
-                }
-            }
         }
 
-        private WWW BuildWWW ( string email )
+        private string BuildUrl ()
         {
-            var headers = new Dictionary<string,string>();
-            headers.Add( "Authorization", "apikey " + _apiKey );
-
-            var data = string.Format( DataFormat, email );
-            var dataBytes = Encoding.ASCII.GetBytes( data );
-
-            var splittedApiKey = _apiKey.Split( '-' );
-
-            if( splittedApiKey.Length != 2 )
-            {
-                Debug.LogError( "MailChimp — Invalid API Key format" );
+            var separatorIndex = _apiKey.LastIndexOf( '-' );
+            if( separatorIndex < 0 )
                 return null;
+
+            var urlPrefix = _apiKey.Substring( separatorIndex + 1 );
+            if( string.IsNullOrEmpty( urlPrefix ) )
+                return null;
+
+            return string.Format( UrlFormat, urlPrefix, _listId );
+        }
+
+        private IEnumerator SendToMailChimp ( string url, string email )
+        {
+            var request = new UnityWebRequest( url, UnityWebRequest.kHttpVerbPOST );
+
+            request.SetRequestHeader( "Content-Type", "application/json" );
+            request.SetRequestHeader( "Authorization", "apikey " + _apiKey );
+            
+            var json = string.Format( JsonFormat, email );
+            var jsonBytes = Encoding.UTF8.GetBytes( json );
+            request.uploadHandler = new UploadHandlerRaw( jsonBytes );
+
+            request.downloadHandler = new DownloadHandlerBuffer();
+
+            yield return request.SendWebRequest();
+
+            if( request.isNetworkError )
+            {
+#if ENABLE_MAILCHIMP_ERROR_LOG
+                Debug.LogErrorFormat( "MailChimp — Network error: {0}", request.error );
+#endif
+                SubscribeError.Invoke( email );
+                yield break;
             }
 
-            var urlPrefix = splittedApiKey[1];
+            if( request.isHttpError )
+            {
+#if ENABLE_MAILCHIMP_ERROR_LOG
+                Debug.LogErrorFormat( "MailChimp — Subscribe error {0}: {1}\n{2}",
+                    request.responseCode, request.error, request.downloadHandler.text );
+#endif
+                SubscribeError.Invoke( email );
+                yield break;
+            }
 
-            var url = string.Format( UrlFormat, urlPrefix, _listId );
-            var www = new WWW( url, dataBytes, headers );
-
-            return www;
+#if ENABLE_MAILCHIMP_SUCCESS_LOG
+            Debug.LogFormat( "MailChimp — Subscribe success {0}\n{1}", request.responseCode, request.downloadHandler.text );
+#endif
+            SubscribeSuccess.Invoke( email );
         }
 
         [Serializable]
